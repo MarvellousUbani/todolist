35b052cbfd1cb988a396f5f2f26a8b376cb5b0ef
Marvellous Ubani <marvellousubani@gmail.com>
2020-06-08 22:49:57 +0100

    added todo functions

--- src/index.js
+++ src/index.js
@@ -18,3 +18,47 @@ addNewTodo.addEventListener('click', e => {
 //   console.log(e.target.className);
 // }
 // addNewTodo.addEventListener('click', displayForm);
+
+// Creating Groups and Group Logic
+
+const groups = {
+  1 : {
+    id: 1,
+    title: "My Travel List",
+    description: "Thing I need to buy",
+    todos: { 12: {title:"swim", description:"Buy Swimsuit", dueDate:"Tomorrow", priority:"High"}, 
+    13: {title:"run", description:"Buy Swimsuit", dueDate:"Tomorrow", priority:"High"},
+   }
+  }
+}
+
+const generateId = () => {
+    const generatedNumber = Math.floor(Math.random() * 100);
+    if(groups.hasOwnProperty(generatedNumber)){
+        return generatedNumber + 1;
+    }
+    return generatedNumber;
+}
+
+const group = (title, description, todos= {}) => {
+    const id = generateId();
+    Groups[id] = {
+        id,
+         title,
+          description,
+          todos,
+  }
+}
+
+const addTodo = (projectId, title, description, dueDate, priority) => {
+    const id = generateId();
+    Groups[projectId].todos[id] = {title, description, dueDate, priority};
+}
+
+const displayTodos = (projectId) => {
+    for(let todo of Groups[projectId].todos){
+        // Add todo stuff to the Dom
+        console.log(todo);
+    }
+}
+
