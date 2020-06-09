af87ede5507d8f7587c77b586ddfd5ee73983061
Marvellous Ubani <marvellousubani@gmail.com>
2020-06-09 21:44:42 +0100

    added addtodofunction

--- src/index.js
+++ src/index.js
@@ -7,13 +7,18 @@ let newListSaveBtn = document.getElementById("newListSaveBtn");
 let listGroup = document.getElementById("listName");
 let linkList = document.getElementById("link-list");
 let btns = [addNewList, addNewTodo];
+let currentGroupId = "";
+const saveBtn = document.querySelector(".save-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const todoForm = document.getElementById("todo-form");
+const newToDo =   document.getElementsByClassName('new-todo')[0];
 
 addNewList.addEventListener('click', e => {
     document.getElementsByClassName('new-list')[0].style.display = "block";
 })
 
 addNewTodo.addEventListener('click', e => {
-    document.getElementsByClassName('new-todo')[0].style.display = 'block';
+  newToDo.style.display = 'block';
 })
 
 
@@ -41,58 +46,68 @@ const group = (title, description, todos = {}) => {
     }
 }
 
-const addTodo = (projectId, title, description, dueDate, priority) => {
+const addTodo = (projectId) => {
     const id = generateId();
+    const title = document.getElementById("title").value;
+    const dueDate = document.getElementById("dateDue").value;
+    const priority = document.getElementById("priority").value;
+    const description = document.getElementById("description").value;
     groups[projectId].todos[id] = { title, description, dueDate, priority };
+    localStorage.setItem("groups", JSON.stringify(groups));
 }
 
 const displayTodos = (projectId) => {
-    const todos = groups[projectId].todos;
     const todoBox = document.getElementById("todo-box");
-    for (let todo in todos) {
-        const { title, description, priority, dueDate } = todos[todo];
-
-        const todoDisplay = document.createElement("div");
-        todoDisplay.classList.add("todo-display");
-
-        const inputHolder = document.createElement("div");
-        const checkbox = document.createElement("input");
-        checkbox.setAttribute("type", "checkbox");
-        const todoTitle = document.createElement("span");
-        todoTitle.classList.add("todoTitle");
-        const moreButton = document.createElement("button");
-        moreButton.textContent = "More...";
-
-        inputHolder.appendChild(checkbox);
-        inputHolder.appendChild(todoTitle);
-        inputHolder.appendChild(moreButton);
-
-        const moreInfo = document.createElement("div");
-        const infoTitle = document.createElement("p");
-        infoTitle.textContent = `${title}`;
-        const infoDescription = document.createElement("p");
-        infoDescription.textContent = `${description}`;
-        const duedate = document.createElement("p");
-        duedate.textContent = `${dueDate}`;
-        const priorityLevel = document.createElement("p");
-        priorityLevel.textContent = `${priority}`;
-
-        const editButton = document.createElement("button");
-        editButton.textContent = "Edit";
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-
-        moreInfo.appendChild(infoTitle);
-        moreInfo.appendChild(infoDescription);
-        moreInfo.appendChild(duedate);
-        moreInfo.appendChild(priorityLevel);
-        moreInfo.appendChild(editButton);
-        moreInfo.appendChild(deleteButton);
-
-        todoDisplay.appendChild(inputHolder);
-        todoDisplay.appendChild(moreInfo);
-
-        todoBox.appendChild(todoDisplay);
+    groups = JSON.parse(localStorage.getItem("groups")) || {};
+    const todos = groups[projectId].todos;
+    if(Object.values(todos).length > 0 ){
+        for (let todo in todos) {
+            const { title, description, priority, dueDate } = todos[todo];
+    
+            const todoDisplay = document.createElement("div");
+            todoDisplay.classList.add("todo-display");
+    
+            const inputHolder = document.createElement("div");
+            const checkbox = document.createElement("input");
+            checkbox.setAttribute("type", "checkbox");
+            const todoTitle = document.createElement("span");
+            todoTitle.classList.add("todoTitle");
+            const moreButton = document.createElement("button");
+            moreButton.textContent = "More...";
+    
+            inputHolder.appendChild(checkbox);
+            inputHolder.appendChild(todoTitle);
+            inputHolder.appendChild(moreButton);
+    
+            const moreInfo = document.createElement("div");
+            const infoTitle = document.createElement("p");
+            infoTitle.textContent = `${title}`;
+            const infoDescription = document.createElement("p");
+            infoDescription.textContent = `${description}`;
+            const duedate = document.createElement("p");
+            duedate.textContent = `${dueDate}`;
+            const priorityLevel = document.createElement("p");
+            priorityLevel.textContent = `${priority}`;
+    
+            const editButton = document.createElement("button");
+            editButton.textContent = "Edit";
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+    
+            moreInfo.appendChild(infoTitle);
+            moreInfo.appendChild(infoDescription);
+            moreInfo.appendChild(duedate);
+            moreInfo.appendChild(priorityLevel);
+            moreInfo.appendChild(editButton);
+            moreInfo.appendChild(deleteButton);
+    
+            todoDisplay.appendChild(inputHolder);
+            todoDisplay.appendChild(moreInfo);
+    
+            todoBox.appendChild(todoDisplay);
+        }
+    }else{
+        todoBox.innerHTML = "No Todos Yet";
     }
 };
 
@@ -101,6 +116,7 @@ const displayGroups = () => {
     for (let group in items) {
         let newDiv = document.createElement("a");
         newDiv.setAttribute("class", "list-tag");
+        newDiv.setAttribute("id", `${items[group].id}`)
         newDiv.textContent = items[group].title;
         newDiv.href = "#";
         linkList.appendChild(newDiv);
@@ -114,7 +130,7 @@ function addToTheDom(e) {
     e.preventDefault();
     let listTitle = document.getElementById("new-list-title").value;
     let listDescription = document.getElementById("list-description").value;
-    groups = JSON.parse(localStorage.getItem("groups"));
+    groups = JSON.parse(localStorage.getItem("groups")) || {};
     group(listTitle, listDescription);
     localStorage.setItem("groups", JSON.stringify(groups));
     linkList.textContent = "";
@@ -122,4 +138,17 @@ function addToTheDom(e) {
     document.getElementsByClassName('new-list')[0].style.display = "none";
     newListForm.reset();
 }
+
 displayGroups();
+
+linkList.addEventListener("click", e => {
+    displayTodos(e.target.id);
+    currentGroupId = e.target.id;
+})
+
+saveBtn.addEventListener("click", () => {
+    addTodo(currentGroupId);
+    todoForm.reset();
+    newToDo.style.display = 'none';
+    displayTodos(currentGroupId);
+});
